0bed99630f5a5cf1ec60f2290fa6087fba26fc09
Azhar Shah <azharshahstl@yahoo.com>
2018-07-18 19:41:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,13 @@
 def prime?(integer)
   if integer <= 0 
     false 
-  end
-  array = (1...(integer)).to_a 
-  if array.count {|x| integer % x = 0} > 2 
-    return false 
   else
+    array = (1...(integer)).to_a 
+    if array.count {|x| integer % x = 0} > 2 
+    return false 
+    else
     return true 
+    end
   end
  
 end
