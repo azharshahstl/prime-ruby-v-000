86c2b9480dd300e24c618054bd157d11cd20ef02
Azhar Shah <azharshahstl@yahoo.com>
2018-07-18 22:05:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   if integer <= 1 
     return false 
   else
-    array = (2...(integer)).to_a 
+    array = (1...(integer)).to_a 
     array.any? {|x| integer % x == 0} 
   end
  
